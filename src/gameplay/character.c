d1f243cd72e936bbd29892c2f16a05538bf745c3
Julien Salies <J.Salies@localhost.localdomain>
2019-04-16 05:30:41 -0400

    [MODIFIED] character.c / new funtion with the conding style

--- src/gameplay/character.c
+++ src/gameplay/character.c
@@ -4,15 +4,36 @@
 ** File description:
 ** function to the gameplay
 */
-
 #include "my.h"
 #include "rpg.h"
 
+void bottom_left_corner(gameplay_t *gameplay)
+{
+    sfView_reset(gameplay->view, (sfFloatRect){3008 - 1920, 0,
+    1920, 1080});
+}
+
 void move_vue(gameplay_t *gameplay, global_t *global)
 {
-    sfView_reset(gameplay->view, (sfFloatRect){0, 0, 1920, 1080});
-    sfView_reset(gameplay->view, (sfFloatRect){gameplay->y - ((1920 / 2) - 64),
-    gameplay->x - ((1080 / 2) - 64), 1920, 1080});
+    if ((gameplay->x > 3008 - 1920 / 2 - 48) && (gameplay->y < 1080 / 2 - 48))
+        bottom_left_corner(gameplay);
+    else if ((gameplay->x < 0 + 1920 / 2 - 48) &&
+            (gameplay->y < 1080 / 2 - 48))
+        sfView_reset(gameplay->view, (sfFloatRect){0, 0, 1920, 1080});
+    else if ((gameplay->x < 0 + 1920 / 2 - 48) &&
+            (gameplay->y > 2000 - 1080 / 2 - 48))
+        top_right_corner(gameplay);
+    else if ((gameplay->x > 3008 - 1920 / 2 - 48) &&
+            (gameplay->y > 2000 - 1080 / 2 - 48))
+        bottom_right_corner(gameplay);
+    else if (gameplay->x > 3008 - (1920 / 2) - 48 ||
+            gameplay->x < (1920 / 2) - 48)
+        camera_horizontal(gameplay);
+    else if (gameplay->y > 2000 - (1080 / 2) - 48 ||
+           gameplay->y < (1080 / 2) - 48)
+        vertical_camera(gameplay);
+    else
+        camera_center(gameplay);
     sfRenderWindow_setView(global->window, gameplay->view);
 }
 
@@ -24,78 +45,6 @@ void move_rect(gameplay_t *gameplay)
     }
     if (gameplay->rect_man.left > 528)
         gameplay->rect_man.left = 432;
-}
-
-void move_up(gameplay_t *gameplay, global_t *global)
-{
-    gameplay->rect_man.top = 144;
-    while (sfKeyboard_isKeyPressed(sfKeyUp)) {
-        move_rect(gameplay);
-        move_vue(gameplay, global);
-        sfSprite_setTextureRect(gameplay->sprite_man, gameplay->rect_man);
-        sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->y, gameplay->x});
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_backg, NULL);
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_man, NULL);
-        sfRenderWindow_display(global->window);
-        gameplay->x -= 5;
-    }
-}
-
-void move_down(gameplay_t *gameplay, global_t *global)
-{
-    gameplay->rect_man.top = 0;
-    while (sfKeyboard_isKeyPressed(sfKeyDown)) {
-        move_rect(gameplay);
-        move_vue(gameplay, global);
-        sfSprite_setTextureRect(gameplay->sprite_man, gameplay->rect_man);
-        sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->y, gameplay->x});
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_backg, NULL);
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_man, NULL);
-        sfRenderWindow_display(global->window);
-        gameplay->x += 5;
-    }
-}
-
-void move_left(gameplay_t *gameplay, global_t *global)
-{
-    gameplay->rect_man.top = 48;
-    while (sfKeyboard_isKeyPressed(sfKeyLeft)) {
-        move_rect(gameplay);
-        move_vue(gameplay, global);
-        sfSprite_setTextureRect(gameplay->sprite_man, gameplay->rect_man);
-        sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->y, gameplay->x});
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_backg, NULL);
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_man, NULL);
-        sfRenderWindow_display(global->window);
-        gameplay->y -= 5;
-    }
-}
-
-void move_right(gameplay_t *gameplay, global_t *global)
-{
-    gameplay->rect_man.top = 96;
-    while (sfKeyboard_isKeyPressed(sfKeyRight)) {
-        move_rect(gameplay);
-        move_vue(gameplay, global);
-        sfSprite_setTextureRect(gameplay->sprite_man, gameplay->rect_man);
-        sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->y, gameplay->x});
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_backg, NULL);
-        sfRenderWindow_drawSprite(global->window, gameplay->sprite_man, NULL);
-        sfRenderWindow_display(global->window);
-        gameplay->y += 5;
-    }
-}
-
-void move_character(gameplay_t *gameplay, global_t *global)
-{
-    if (global->event.key.code == sfKeyUp)
-        move_up(gameplay, global);
-    if (global->event.key.code == sfKeyDown)
-        move_down(gameplay, global);
-    if (global->event.key.code == sfKeyLeft)
-        move_left(gameplay, global);
-    if (global->event.key.code == sfKeyRight)
-        move_right(gameplay, global);
 }
 
 int check_events(global_t *global, gameplay_t *gameplay)
@@ -120,8 +69,8 @@ void set_my_rect(gameplay_t *gameplay)
 
 void set_position(gameplay_t *gameplay)
 {
-    sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->y, gameplay->x});
-    sfSprite_setScale(gameplay->sprite_man, (sfVector2f){2, 2});
+    sfSprite_setPosition(gameplay->sprite_man, (sfVector2f){gameplay->x, gameplay->y});
+    sfSprite_setScale(gameplay->sprite_man, (sfVector2f){2.08333, 2.08333});
 }
 
 void init_texture(gameplay_t *gameplay)
