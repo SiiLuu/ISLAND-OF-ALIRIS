9f3190259bb5ad725c7b35647077d5c23f1ff084
Leyton Ly <leyton.ly@epitech.eu>
2019-04-24 22:04:36 +0200

    [ADDED] src/gameplay/manage_collision.c / Manage the colisions with two blocks width (diagonal)

---
+++ src/gameplay/manage_collision.c
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2019
+** collision.c
+** File description:
+** manage the collision of the player on the map
+*/
+
+#include <stdbool.h>
+#include "my.h"
+#include "rpg.h"
+
+bool manage_col_up(gameplay_t *gameplay, int pos_x, int mpos_y)
+{
+    int mpos_x = (pos_x + 99) / 100;
+
+    if (mpos_x <= 59) {
+        if ((mpos_x == mpos_x / 100) ||
+            gameplay->map[mpos_y][mpos_x] == '0')
+            return (true);
+    }
+    return (false);
+}
+
+bool manage_col_down(gameplay_t *gameplay, int pos_x, int mpos_y)
+{
+    int mpos_x = (pos_x + 99) / 100;
+
+    if (mpos_x <= 59) {
+        if ((mpos_x == mpos_x / 100) ||
+            gameplay->map[mpos_y][mpos_x] == '0')
+            return (true);
+    }
+    return (false);
+}
+
+bool manage_col_left(gameplay_t *gameplay, int mpos_x, int pos_y)
+{
+    int mpos_y = (pos_y + 99) / 100;
+
+    if (mpos_y <= 59) {
+        if ((mpos_y == pos_y / 100) ||
+            gameplay->map[mpos_y][mpos_x] == '0')
+            return (true);
+    }
+    return (false);
+}
+
+bool manage_col_right(gameplay_t *gameplay, int mpos_x, int pos_y)
+{
+    int mpos_y = (pos_y + 99) / 100;
+
+    if (mpos_y <= 59) {
+        if ((mpos_y == pos_y / 100) ||
+            gameplay->map[mpos_y][mpos_x] == '0')
+            return (true);
+    }
+    return (false);
+}
