423b9b2ea67423a4c731498dd68371fec410a490
Julien Salies <J.Salies@localhost.localdomain>
2019-05-01 06:31:36 -0400

    [MODIFIED] fight folder / new win gestion

--- src/fight/detect_win.c
+++ src/fight/detect_win.c
@@ -18,6 +18,16 @@ void player_loose_display(global_t *global)
     sfRenderWindow_drawSprite(global->window, global->fight->players, NULL);
 }
 
+void player_win_display(global_t *global)
+{
+    sfSprite_setTextureRect(global->fight->players, global->fight->rect);
+    sfSprite_setColor(global->fight->boss1s, sfTransparent);
+    sfRenderWindow_clear(global->window, sfBlack);
+    sfRenderWindow_drawSprite(global->window, global->fight->wp1s, NULL);
+    sfRenderWindow_drawSprite(global->window, global->fight->boss1s, NULL);
+    sfRenderWindow_drawSprite(global->window, global->fight->players, NULL);
+}
+
 void player_loose(global_t *global)
 {
     sfFont* font = sfFont_createFromFile("resource/font/text.ttf");
@@ -42,13 +52,39 @@ void player_loose(global_t *global)
     sfClock_destroy(clocks);
 }
 
+void player_win(global_t *global)
+{
+    sfFont* font = sfFont_createFromFile("resource/font/text.ttf");
+    sfText* text = sfText_create();
+    sfClock *clocks = sfClock_create();
+
+    sfText_setString(text, "VICTORY !");
+    sfText_setFont(text, font);
+    sfText_setColor(text, sfBlack);
+    sfText_setCharacterSize(text, 350);
+    sfText_setPosition(text, (sfVector2f){300, 100});
+    global->fight->rect.left = 448;
+    global->fight->rect.top = 64;
+    player_win_display(global);
+    sfRenderWindow_drawText(global->window, text, NULL);
+    draw_fight_info(global);
+    sfRenderWindow_display(global->window);
+    while (sfTime_asMilliseconds(sfClock_getElapsedTime(
+            global->fight->clocks)) < 10000);
+    sfFont_destroy(font);
+    sfText_destroy(text);
+    sfClock_destroy(clocks);
+}
+
 int dectect_win(global_t *global)
 {
     if (global->fight->life1 <= 0) {
         player_loose(global);
         exit (0);
     }
-    if (global->fight->life2 <= 0)
+    if (global->fight->life2 <= 0) {
+        player_win(global);
         return (1);
+    }
     return (0);
 }
