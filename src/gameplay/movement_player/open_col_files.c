3245c0c7200683fb22d59974c6ea09a385426738
Leyton Ly <leyton.ly@epitech.eu>
2019-05-01 17:01:01 +0200

    [ADDED] src/gameplay/movement_player/open_col_files.c / Stock the collision files in array of strings

---
+++ src/gameplay/movement_player/open_col_files.c
@@ -0,0 +1,48 @@
+/*
+** EPITECH PROJECT, 2019
+** open_col_files.c
+** File description:
+** my_defender, main function
+*/
+
+#include "my.h"
+#include "rpg.h"
+
+
+char **get_map(char *path, char **map)
+{
+    char *buffer = NULL;
+    int fd = 0;
+    int i = 0;
+    struct stat filepath;
+
+    stat(path, &filepath);
+    fd = open(path, O_RDONLY);
+    if (fd == -1)
+        return (NULL);
+    i = filepath.st_size;
+    buffer = malloc(sizeof(char) * i);
+    if (buffer == NULL)
+        return (NULL);
+    read(fd, buffer, i);
+    close(fd);
+    map = string_to_tab(buffer);
+    free (buffer);
+    return (map);
+}
+
+int open_file(global_t *global)
+{
+
+    global->gameplay->wmap = get_map("resource/CollisionWM.txt",
+                    global->gameplay->wmap);
+    global->gameplay->mapc_fcp = get_map("resource/CollisionFCP.txt",
+                    global->gameplay->mapc_fcp);
+    global->gameplay->mapc_vil = get_map("resource/CollisionVil.txt",
+                    global->gameplay->mapc_vil);
+    global->gameplay->map = global->gameplay->wmap;
+    if (global->gameplay->wmap == NULL || 
+    global->gameplay->mapc_fcp == NULL || global->gameplay->mapc_vil == NULL)
+        return (84);
+    return (0);
+}
