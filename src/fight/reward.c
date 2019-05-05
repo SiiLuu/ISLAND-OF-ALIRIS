1a844118fdf1d1b64b8e5cb3bcf12416c3288b96
Lucas Simao <lsimao@Lucas.simao>
2019-05-05 16:28:19 +0200

    [MODIF] src/fight/reward.c / fix the gift message in fight.

--- src/fight/reward.c
+++ src/fight/reward.c
@@ -8,12 +8,10 @@
 #include "my.h"
 #include "rpg.h"
 
-char *reward_str(global_t *global)
+char *reward_str2(global_t *global)
 {
     static bool final = 0;
     static bool winter = 0;
-    static bool desert = 0;
-    static bool hl = 0;
 
     if (global->gameplay->boss->win_vs_final_boss == 1 && final == 0) {
         final = 1;
@@ -21,16 +19,28 @@ char *reward_str(global_t *global)
     }
     if (global->gameplay->boss->win_vs_winter_boss == 1 && winter == 0) {
         winter = 1;
-        return ("Marauder's ax obtained !!");
+        return ("adaptive helmet obtained !!");
     }
+    return (NULL);
+}
+
+char *reward_str(global_t *global)
+{
+    static bool desert = 0;
+    static bool hl = 0;
+    char *string = NULL;
+
+    if ((string = reward_str2(global)) != NULL)
+        return (string);
     if (global->gameplay->boss->win_vs_desert_boss == 1 && desert == 0) {
         desert = 1;
-        return ("adaptive helmet obtained !!");
+        return ("enchanted breastplate obtained !!");
     }
     if (global->gameplay->boss->win_vs_hl_boss == 1 && hl == 0) {
         hl = 1;
-        return ("enchanted breastplate obtained !!");
+        return ("Marauder's ax obtained !!");
     }
+    return (NULL);
 }
 
 void reward(global_t *global)
