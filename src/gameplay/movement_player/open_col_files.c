e354b106ea7f1ec77cd2b52f1d510f41e9340fcd
Leyton Ly <leyton.ly@epitech.eu>
2019-05-05 20:00:38 +0200

    [MODIFIED] src/gameplay/movement_player/open_col_files.c / Added cols files to open

--- src/gameplay/movement_player/open_col_files.c
+++ src/gameplay/movement_player/open_col_files.c
@@ -8,6 +8,36 @@
 #include "my.h"
 #include "rpg.h"
 
+void free_file(char **map)
+{
+    int i = 0;
+
+    for (i = 0; map[i] != NULL; i++)
+        free(map[i]);
+    free(map);
+}
+
+int open_file_dungeons(global_t *global)
+{
+    global->gameplay->mapc_vol = get_map("resource/CollisionVolc.txt",
+                    global->gameplay->mapc_vol);
+    global->gameplay->mapc_inpy = get_map("resource/CollisionInPy.txt",
+                    global->gameplay->mapc_inpy);
+    global->gameplay->mapc_outpy = get_map("resource/CollisionOutpy.txt",
+                    global->gameplay->mapc_outpy);
+    return (0);
+}
+
+int open_file_fc(global_t *global)
+{
+    global->gameplay->mapc_fcp = get_map("resource/CollisionFCP.txt",
+                    global->gameplay->mapc_fcp);
+    global->gameplay->mapc_fcn = get_map("resource/CollisionFCN.txt",
+                    global->gameplay->mapc_fcn);
+    global->gameplay->mapc_fcf = get_map("resource/CollisionFCF.txt",
+                    global->gameplay->mapc_fcf);
+    return (0);
+}
 
 char **get_map(char *path, char **map)
 {
@@ -33,20 +63,20 @@ char **get_map(char *path, char **map)
 
 int open_file(global_t *global)
 {
-
     global->gameplay->wmap = get_map("resource/CollisionWM.txt",
                     global->gameplay->wmap);
-    global->gameplay->mapc_fcp = get_map("resource/CollisionFCP.txt",
-                    global->gameplay->mapc_fcp);
     global->gameplay->mapc_vil = get_map("resource/CollisionVil.txt",
                     global->gameplay->mapc_vil);
     global->gameplay->mapc_cas = get_map("resource/CollisionCASL.txt",
                     global->gameplay->wmap);
-    global->gameplay->mapc_vol = get_map("resource/CollisionVolc.txt",
-                    global->gameplay->mapc_vol);
+    global->gameplay->mapc_viln = get_map("resource/CollisionVILN.txt",
+                    global->gameplay->mapc_viln);
+    global->gameplay->mapc_casn = get_map("resource/CollisionCASN.txt",
+                    global->gameplay->mapc_casn);
+    global->gameplay->mapc_fcas = get_map("resource/CollisionCASF.txt",
+                    global->gameplay->mapc_fcas);
     global->gameplay->map = global->gameplay->wmap;
-    if (global->gameplay->wmap == NULL ||
-    global->gameplay->mapc_fcp == NULL || global->gameplay->mapc_vil == NULL)
-        return (84);
+    open_file_fc(global);
+    open_file_dungeons(global);
     return (0);
 }
